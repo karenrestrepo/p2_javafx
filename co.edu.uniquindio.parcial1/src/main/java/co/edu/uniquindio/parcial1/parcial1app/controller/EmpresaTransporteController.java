6ba90e8a61a9c7c2cd1207700e503c6fc7b47995
karenrestrepo <karenv.restrepom@uqvirtual.edu.co>
2024-03-17 17:36:53 -0500

    .

--- co.edu.uniquindio.parcial1/src/main/java/co/edu/uniquindio/parcial1/parcial1app/controller/EmpresaTransporteController.java
+++ co.edu.uniquindio.parcial1/src/main/java/co/edu/uniquindio/parcial1/parcial1app/controller/EmpresaTransporteController.java
@@ -1,4 +1,190 @@
 package co.edu.uniquindio.parcial1.parcial1app.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class EmpresaTransporteController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnCrearPropietario;
+
+    @FXML
+    private Button btnCrearUsuario;
+
+    @FXML
+    private Button btnCrearVehiculoCarga;
+
+    @FXML
+    private Button btnCrearVehiculoTransporte;
+
+    @FXML
+    private Button btnPropietarioMayorEdad;
+
+    @FXML
+    private Button btnTotalPasajerosVehiculoTransporte;
+
+    @FXML
+    private Button btnUsuariosMayoresEdad;
+
+    @FXML
+    private Button btnUsuariosMovilizadosVehiculoTransporte;
+
+    @FXML
+    private Button btnUsuariosPesoSuperior;
+
+    @FXML
+    private TextField txtCapacidadVehiculoCarga;
+
+    @FXML
+    private TextField txtCedulaPropietario;
+
+    @FXML
+    private TextField txtCelularPropietario;
+
+    @FXML
+    private TextField txtChasisVehiculoCarga;
+
+    @FXML
+    private TextField txtChasisVehiculoTransporte;
+
+    @FXML
+    private TextField txtColorVehiculoCarga;
+
+    @FXML
+    private TextField txtColorVehiculoTransporte;
+
+    @FXML
+    private TextField txtEdadPropietario;
+
+    @FXML
+    private TextField txtEdadUsuario;
+
+    @FXML
+    private TextField txtEjesVehiculoCarga;
+
+    @FXML
+    private TextField txtEmailPropietario;
+
+    @FXML
+    private TextField txtMarcaVehiculoCarga;
+
+    @FXML
+    private TextField txtMarcaVehiculoTransporte;
+
+    @FXML
+    private TextField txtMaximoPasajerosVehiculoTransporte;
+
+    @FXML
+    private TextField txtModeloVehiculoCarga;
+
+    @FXML
+    private TextField txtModeloVehiculoTransporte;
+
+    @FXML
+    private TextField txtNombrePropietario;
+
+    @FXML
+    private TextField txtNombreUsuario;
+
+    @FXML
+    private TextField txtPesoUsuario;
+
+    @FXML
+    private TextField txtPesoUsuariosSuperior;
+
+    @FXML
+    private TextField txtPlacaUsuarioVehiculoTransporte;
+
+    @FXML
+    private TextField txtPlacaVehiculoCarga;
+
+    @FXML
+    private TextField txtPlacaVehiculoTransporte;
+
+    @FXML
+    private TextArea txtPropietarioCreado;
+
+    @FXML
+    private TextArea txtResultadoPropietario;
+
+    @FXML
+    private TextArea txtResultadoUsuario;
+
+    @FXML
+    private TextArea txtResultadoVehiculoCarga;
+
+    @FXML
+    private TextArea txtResultadoVehiculoTransporte;
+
+    @FXML
+    private TextArea txtUsuarioCreado;
+
+    @FXML
+    private TextArea txtVehiculoCargaCreado;
+
+    @FXML
+    private TextArea txtVehiculoTransporteCreado;
+
+    @FXML
+    void onCrearPropietario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCrearUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCrearVehiculoCarga(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCrearVehiculoTransporte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPropietarioMayorEdad(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onTotalPasajerosVehiculoTransporte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUsuariosMayoresEdad(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUsuariosMovilizadosVehiculoTransporte(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUsuariosPesoSuperior(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
+
